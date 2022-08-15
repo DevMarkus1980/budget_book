00f43e68d164eaa8781ea31d0b961d573d48ada5
M@4ku$ <82565355+StruMa-Code@users.noreply.github.com>
2022-08-15 10:20:53 +0200

    :bug: Table anzeige korregiert

--- src/main/java/de/struma/budget_book/controller/MainController.java
+++ src/main/java/de/struma/budget_book/controller/MainController.java
@@ -2,6 +2,7 @@ package de.struma.budget_book.controller;
 
 import de.struma.budget_book.service.BuchungsKalkulationService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
