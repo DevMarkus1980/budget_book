b17075c5b6ef4b107c9ae924023afae0f6f70aae
M@4ku$ <82565355+StruMa-Code@users.noreply.github.com>
2022-08-16 17:37:10 +0200

    :recycle: Button geändert

--- src/main/java/de/struma/budget_book/controller/SparController.java
+++ src/main/java/de/struma/budget_book/controller/SparController.java
@@ -3,7 +3,6 @@ package de.struma.budget_book.controller;
 import de.struma.budget_book.model.SparkaufBuchungsModel;
 import de.struma.budget_book.service.SparBuchungsService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
